2e69419fb01dfabaf4c62049eeb27eedcfa2fcb5
leeve1247 <multirkh@gmail.com>
2023-10-01 12:43:56 +0900

    Improve : ShelterUI UGUI로 전환

---
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/ShelterUIScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ShelterUIScript : MonoBehaviour
+{
+    private Button _buttonExit;
+    private Button _buttonStage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var a = GetComponentsInChildren<Button>();
+        foreach (var button in a)
+        {
+            Debug.Log(button.transform.name);
+            var buttonName = button.transform.name; 
+            if (buttonName == "Button_Exit")
+            {
+                _buttonExit = button;
+            } else if (buttonName == "Button_Stage")
+            {
+                _buttonStage = button;
+            }
+        }
+        
+        _buttonStage.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("StageScene");
+        });
+        
+        _buttonExit.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
