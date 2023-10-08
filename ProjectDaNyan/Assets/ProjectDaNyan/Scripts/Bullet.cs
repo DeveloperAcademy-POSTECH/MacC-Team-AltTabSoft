dbc152d8704a50d4fa8727391da4191c9a483f1c
Kiyoung <rlaky45@naver.com>
2023-10-08 17:54:53 +0900

    Add: 초월 공격 방식 구현

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Bullet.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Bullet.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-   
+    public enum Type {Default, Piercing};
+    public Type type;
     public int damage;
+
+    private void Update()
+    {
+        Destroy(gameObject,2);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" && type != Type.Piercing) //관통 타입이 아니면 적에 맞을 경우 총알 오브젝트 사라짐
         {
             //적에 닿았을 때 로직
             Destroy(gameObject);
         }
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "RandomObject")
+        if (collision.gameObject.tag == "RandomObject") //태그명은 추후 변경 필요
         {
             //맵에 생성된 랜덤 오브젝트와 닿았을 때 사라
             Destroy(gameObject);
         }
+
     }
+
+    
 }
--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/PlayerAttack.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/PlayerAttack.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public enum Type { Basic, Upgraded , Laser};//타입은 따로 bullet 스크립트를 만들어서 빼낼 예정
-    public Type type;
     public float basicFireRate; //기본공격주기 
     public float upgradedFireRate; //초월공격주기 
     public float laserFireRate; //관통공격주기 
@@ -13,13 +11,15 @@ public class PlayerAttack : MonoBehaviour
     public float upgradedFireSpeed; //초월공격 투사체 속도
    
     
-    public Transform bulletPosition; //플레이어 오브젝트에 총알이 나갈 위치를 오브젝트로 지정 
+    public Transform bulletPosition; //플레이어 오브젝트에 총알이 나갈 위치를 오브젝트로 지정
+    public Transform[] upgradeBulletPositions; //초월 공격에서 추가될 공격 방향 
     public GameObject basicBullet; //기본 공격 총알로 쓸 오브젝트 프리펩
     public GameObject upgradedBullet; //초월 공격 여러갈래로 퍼져나갈 총알들 프리펩
     public GameObject laserBullet; //레이저 관통 공격 오브젝트 프리
 
     float basicfireDelay;
     bool isFireReady;
+    bool isUpgrade = true;
 
     private void Update()
     {
@@ -28,13 +28,27 @@ public class PlayerAttack : MonoBehaviour
 
     public void Use()
     {
-        isFireReady = basicFireRate < basicfireDelay;
-        basicfireDelay += Time.deltaTime;
-        if (isFireReady)
+        if (isUpgrade == false)
         {
-            StartCoroutine("BasicAttack");
-            basicfireDelay = 0;
+            isFireReady = basicFireRate < basicfireDelay;
+            basicfireDelay += Time.deltaTime;
+            if (isFireReady)
+            {
+                StartCoroutine("BasicAttack");
+                basicfireDelay = 0;
+            }
         }
+        else
+        {
+            isFireReady = upgradedFireRate < basicfireDelay;
+            basicfireDelay += Time.deltaTime;
+            if (isFireReady)
+            {
+                StartCoroutine("UpgradeAttack");
+                basicfireDelay = 0;
+            }
+        }
+        
             
         //StartCoroutine("LaserAttack");
     }
@@ -46,8 +60,41 @@ public class PlayerAttack : MonoBehaviour
         Rigidbody basicBulletRigid = instantBasicBullet.GetComponent<Rigidbody>();
         basicBulletRigid.useGravity = false;
         basicBulletRigid.velocity = bulletPosition.forward * basicFireSpeed;
+        //basicBulletRigid.velocity = new Vector3(1,0,2).normalized * basicFireSpeed;
         yield return null;
         //초월 공격 로
+
+    }
+
+    IEnumerator UpgradeAttack()
+    {
+        GameObject instantBasicBullet = Instantiate(upgradedBullet, bulletPosition.position, Quaternion.identity);
+        Rigidbody upgradedBulletRigid = instantBasicBullet.GetComponent<Rigidbody>();
+        upgradedBulletRigid.useGravity = false;
+        upgradedBulletRigid.velocity = bulletPosition.forward * upgradedFireSpeed;
+
+        GameObject instantBasicBullet1 = Instantiate(upgradedBullet, upgradeBulletPositions[0].position, Quaternion.identity);
+        Rigidbody upgradedBulletRigid1 = instantBasicBullet1.GetComponent<Rigidbody>();
+        upgradedBulletRigid1.useGravity = false;
+        upgradedBulletRigid1.velocity = upgradeBulletPositions[0].forward * upgradedFireSpeed;
+
+        GameObject instantBasicBullet2 = Instantiate(upgradedBullet, upgradeBulletPositions[1].position, Quaternion.identity);
+        Rigidbody upgradedBulletRigid2 = instantBasicBullet2.GetComponent<Rigidbody>();
+        upgradedBulletRigid2.useGravity = false;
+        upgradedBulletRigid2.velocity = upgradeBulletPositions[1].forward * upgradedFireSpeed;
+
+        GameObject instantBasicBullet3 = Instantiate(upgradedBullet, upgradeBulletPositions[2].position, Quaternion.identity);
+        Rigidbody upgradedBulletRigid3 = instantBasicBullet3.GetComponent<Rigidbody>();
+        upgradedBulletRigid3.useGravity = false;
+        upgradedBulletRigid3.velocity = upgradeBulletPositions[2].forward * upgradedFireSpeed;
+
+        GameObject instantBasicBullet4 = Instantiate(upgradedBullet, upgradeBulletPositions[3].position, Quaternion.identity);
+        Rigidbody upgradedBulletRigid4 = instantBasicBullet4.GetComponent<Rigidbody>();
+        upgradedBulletRigid4.useGravity = false;
+        upgradedBulletRigid4.velocity = upgradeBulletPositions[3].forward * upgradedFireSpeed;
+
+        yield return null;
+
     }
 
     IEnumerator LaserAttack()
