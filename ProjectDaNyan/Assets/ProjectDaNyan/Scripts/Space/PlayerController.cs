ecb9391941315cbfa49d983dc83017d7092f8caf
Rubik <67681580+RubiksCube33@users.noreply.github.com>
2023-10-12 15:37:34 +0900

    Add: 벽에 대시로 닿으면 반사, 바위에 대시로 닿으면 올라가서 대시로 탈출 가능 (#23)

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/PlayerController.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/PlayerController.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] private GameObject playerObject;
     [SerializeField] private GameObject playerMoveDirectionObject;
     [SerializeField] private GameObject playerDashDirectionObject;
@@ -12,10 +14,13 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private JoystickController _joy;
     
+    [SerializeField] private float rockHeight;
+    
     private Rigidbody playerRigid;
     private TrailRenderer playerTrailRenderer;
     private Animator playerAnim;
     private CharacterController playerCharacterController;
+    private Transform playerTransform;
 
     private Vector3 movePosition;
     private Vector3 dashMovePosition;
@@ -30,12 +35,13 @@ public class PlayerController : MonoBehaviour
     private float playerRotationPositionX;
     private float playerRotationPositionY;
     private float _floatingPosition;
-
+    
     void Start()
     {
         playerTrailRenderer = playerObject.GetComponent<TrailRenderer>();
         playerAnim = playerObject.GetComponent<Animator>();
         playerCharacterController = playerObject.GetComponent<CharacterController>();
+        playerTransform = playerObject.GetComponent<Transform>();
 
         // 게임이 시작될 때 캐릭터의 머리가 카메라를 바라보도록 회전각을 설정
         playerRotationPositionX = 1;
@@ -61,6 +67,13 @@ public class PlayerController : MonoBehaviour
                 PlayerDash();
                 break;
             }
+            case JoystickController.PlayerState.onTheRock:
+                PlayerMovingControllOnTheRock();
+                break;
+            case JoystickController.PlayerState.exitDashFromRock:
+                transform.position = new Vector3(transform.position.x, transform.position.y - rockHeight, transform.position.z);
+                PlayerDash();
+                break;
             default:
                 PlayerStop();
                 break;
@@ -101,15 +114,15 @@ public class PlayerController : MonoBehaviour
 
     void PlayerDash()
     {
-            playerCharacterController.Move(new Vector3(dashMovePosition.x,_floatingPosition,dashMovePosition.z));
-            dashTimerCount += 1;
+        playerCharacterController.Move(new Vector3(dashMovePosition.x,_floatingPosition,dashMovePosition.z));
+        dashTimerCount += 1;
             
-            if (dashTimerCount == dashLimitTic1SecondsTo50)
-            {
-                dashTimerCount = 0;
-                _joy.playerState = JoystickController.PlayerState.stop;
-            }
-            playerDashDirectionObject.SetActive(false);
+        if (dashTimerCount == dashLimitTic1SecondsTo50)
+        {
+            dashTimerCount = 0;
+            _joy.playerState = JoystickController.PlayerState.stop;
+        }
+        playerDashDirectionObject.SetActive(false);
     }
 
     void PlayerFall()
@@ -123,5 +136,74 @@ public class PlayerController : MonoBehaviour
             _floatingPosition = 0f;
         }
     }
+
+    void PlayerMovingControllOnTheRock()
+    {
+        Vector3 normalized = new Vector3(_joy.Horizontal+_joy.Vertical, 0, _joy.Vertical-_joy.Horizontal).normalized;
+        dashMovePosition = normalized * (Time.deltaTime * playerSpeed * dashSpeed);
+    }
+
+    void PlayerWallReflection(Collision wall)
+    {
+        dashTimerCount = 0;
+        Vector3 normal = wall.contacts[0].normal; //법선벡터
+        playerCharacterController.Move(new Vector3(-dashMovePosition.x,0,-dashMovePosition.z));
+        dashMovePosition = Vector3.Reflect(dashMovePosition, normal);
+    }
+
+    void PlayerMoveOnTheRock(Collision rock)
+    {
+        _joy.playerState = JoystickController.PlayerState.onTheRock;
+        transform.position = (new Vector3(rock.transform.position.x,rock.transform.position.y + rockHeight,rock.transform.position.z));
+    }
+
+    //충돌 시 뚫고 나갈 수 없는 물체에 닿았을 때 작동 (벽, 돌 등)
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Debug.Log("Wall");
+            if (_joy.playerState == JoystickController.PlayerState.dash)
+            {
+                PlayerWallReflection(other);
+            }
+        }
+        else if (other.gameObject.tag == "Rock")
+        {
+            Debug.Log("Rock");
+            if ((_joy.playerState != JoystickController.PlayerState.onTheRock) && _joy.playerState != JoystickController.PlayerState.exitDashFromRock)
+            {
+                if (_joy.playerState == JoystickController.PlayerState.dash)
+                {
+                    PlayerMoveOnTheRock(other);
+                }
+            }
+        }
+    }
     
+    //충돌 시 뚫고 나갈 수 없는 물체에 닿은 상태를 유지할 때 작동 (벽, 돌 등)
+    //이게 없으면 캐릭터가 벽이랑 붙은 상태에서 벽으로 대시할 때 대시 판정이 발생하지 않음
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Debug.Log("In Wall");
+            if (_joy.playerState == JoystickController.PlayerState.dash)
+            {
+                dashTimerCount = 0;
+                playerCharacterController.Move(new Vector3(-dashMovePosition.x,0,-dashMovePosition.z));
+                dashMovePosition = new Vector3(-dashMovePosition.x, 0, -dashMovePosition.z);
+            }
+        }
+        // else if (other.gameObject.tag == "Rock")
+        // {
+        //     if (_joy.playerState != JoystickController.PlayerState.onTheRock)
+        //     {
+        //         if (_joy.playerState == JoystickController.PlayerState.dash)
+        //         {
+        //             PlayerMoveOnTheRock(other);
+        //         }
+        //     }
+        // }
+    }
 }
