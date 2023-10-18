1eb2b430b05ca25e76dab51f1e929d4ea87eff56
leeve1247 <multirkh@gmail.com>
2023-10-18 11:53:04 +0900

    Improve: 로직 개선

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/PlayerLevelUpSkillBoxButton.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/PlayerLevelUpSkillBoxButton.cs
@@ -15,8 +15,8 @@ public class PlayerLevelupSkillBoxButton : MonoBehaviour
     }
     public void SelectSkillBox()
     {
-        Debug.Log("왜 안됨");
-        _playerStatus.Player_now_EXP = 0;
+        var overFlowedExp = _playerStatus.Player_now_EXP - _playerStatus.Level_Up_Require_EXP ;
+        _playerStatus.Player_now_EXP = overFlowedExp; // 레벨에 필요한 만큼만 경험치를 제외, 연속적인 레벨업 가능
         skillUI.SetActive(false);
         GameManager.Inst.ResumeGame();
         
