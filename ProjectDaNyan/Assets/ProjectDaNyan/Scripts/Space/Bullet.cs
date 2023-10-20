8b36e1ef47a4e9d9e8ce7de7c72643cb1813824d
Kiyoung <rlaky45@naver.com>
2023-10-20 09:31:50 +0900

    Delete: 불필요한 씬과 주석 삭제

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
@@ -19,23 +19,9 @@ public class Bullet : MonoBehaviour
         StartCoroutine(Goodbye());
     }
 
-    private void Update()
-    {
-        //if (type != Type.Laser)
-        //    Destroy(gameObject, 1f);
-
-    }
-
 
     IEnumerator Goodbye()
     {
-
-
-        //while(time > 0)
-        //{
-        //    time -= 1;
-        //    yield return new WaitForSeconds(1);
-        //}
         if (type != Type.Field)
         {
             yield return new WaitForSeconds(1f);
@@ -62,21 +48,5 @@ public class Bullet : MonoBehaviour
             trail.Clear();
             ObjectPoolManager.Inst.DestroyObject(this.gameObject);
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Enemy" && type != Type.Piercing && type != Type.Laser) //관통 타입이 아니면 적에 맞을 경우 총알 오브젝트 사라
-        {
-            //적에 닿았을 때 로직
-            Destroy(gameObject);
-        }
-
-        if (collision.gameObject.tag == "RandomObject" && type != Type.Laser) //태그명은 추후 변경 필요
-        {
-            //맵에 생성된 랜덤 오브젝트와 닿았을 때 사라
-            Destroy(gameObject);
-        }
-
     }
 }
