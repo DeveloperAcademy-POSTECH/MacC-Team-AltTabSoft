4b43bf8a33c07085512cc9845a5122a55ac87954
NewYorkKim <newyorkkim1030@gmail.com>
2023-10-30 21:23:04 +0900

    Add: 아이템 랜덤 드랍 구현 (#83)

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/ItemController.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/ItemController.cs
@@ -1,23 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ItemController : MonoBehaviour
 {
-    /*private MapManager mapManager;
-
-    private void Start()
+    private static ItemController inst = null;
+    public static ItemController Inst
     {
-        mapManager = FindObjectOfType<MapManager>();
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        get
         {
-            //mapManager.collectedCats++;
-            gameObject.SetActive(false);
-            Debug.Log("획득한 고양이 수: " +  mapManager.collectedCats);
+            if (inst == null)
+            {
+                // 인스턴스가 없을 경우, 현재 Scene에서 찾아봅니다.
+                inst = FindObjectOfType<ItemController>();
+            }
+            return inst;
         }
-    }*/
+    }
+
+    private void Awake()
+    {
+        if (inst == null)
+        {
+            inst = this; // 인스턴스를 할당합니다.
+        }
+    }
+
+    public void DropItem()
+    {
+        var random = Random.Range(0, 101);
+
+        if (random <= 75)
+        {
+            DropGold();
+        }
+        else
+        {
+            DropEnergy();
+        }
+    }
+
+    private void DropGold()
+    { 
+        var random = Random.Range(0, 101);
+
+        if (random <= 20)
+        {
+            Debug.Log("LargeCoin 획득");
+        }
+        else if (random <= 50)
+        {
+            Debug.Log("MiddleCoin 획득");
+        }
+        else
+        {
+            Debug.Log("SmallCoin 획득");
+        }
+    }
+
+    private void DropEnergy()
+    {
+        Debug.Log("Energy 획득");
+    }
 }
 
