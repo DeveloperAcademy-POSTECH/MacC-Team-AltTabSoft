c5b625c18fe12c29b6b1162c332077675c5e34b4
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-11-07 01:57:16 +0900

    Fix: JoystickControllerEditor.cs

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/ControllerUI/Editor/JoystickControllerEditor.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/ControllerUI/Editor/JoystickControllerEditor.cs
@@ -6,24 +6,19 @@ using UnityEditor;
 [CustomEditor(typeof(JoystickController),true)]
 public class JoystickControllerEditor : FloatingJoystickEditor
 {
-    private SerializedProperty _playerState;
-    private SerializedProperty _playerData;
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        PlayerState _playerState;
     }
 
     protected override void OnEnable()
     {
         base.OnEnable();
-        _playerState = serializedObject.FindProperty("_playerState");
     }
 
     protected override void DrawComponents()
     {
         base.DrawComponents();
-        EditorGUILayout.ObjectField(_playerState, new GUIContent("PlayerState","PlayerStatus"));
     }
 }
