47db1ea22bf9e7af34946c455d60b0810efc0db6
Kiyoung <rlaky45@naver.com>
2023-11-07 19:18:46 +0900

    Improve: 총알 피격시 불꽃 효과 및 폭탄 공격 트레일 문제해결

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private AttackStatus _attackStatus;
     [SerializeField] private GameObject _bomb;
+    [SerializeField] private GameObject _explosion;
 
     public enum Type {Default, Upgrade, Laser, Field, Bomb, BombBlast, Drone};
     public Type type;
@@ -46,7 +47,7 @@ public class Bullet : MonoBehaviour
 
     IEnumerator Goodbye()
     {
-        if (type != Type.Field && type != Type.Laser && type != Type.Bomb)
+        if (type != Type.Field && type != Type.Laser)
         {
             yield return new WaitForSeconds(1f);
             trail.Clear();
@@ -63,14 +64,22 @@ public class Bullet : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Monster") && type != Type.BombBlast)
         {
-            if(type != Type.Laser && type != Type.Bomb)
+            if(type != Type.Laser)
                 trail.Clear();
+            if(type == Type.Upgrade) //몬스터 피격시 폭발 효과낼 총알 타입 조건 추가 필요
+            {
+                GameObject afterExplosion = ObjectPoolManager.Inst.BringObject(_explosion);
+                afterExplosion.transform.position = this.gameObject.transform.position;
+            }
+            
             ObjectPoolManager.Inst.DestroyObject(this.gameObject);
+            
 
             //폭탄총알이 몹에게 닿으면 몹에게 폭탄이 부착됨
             if(type == Type.Bomb && other != null && other.transform.Find("BombOnMonster") == null)
             {
                 GameObject InstantBomb = ObjectPoolManager.Inst.BringObject(_bomb);
+                Debug.Log("폭탄 부착");
                 InstantBomb.transform.parent = other.transform;
                 InstantBomb.transform.position = other.transform.position + new Vector3(0, 1.9f, 0);
             }
