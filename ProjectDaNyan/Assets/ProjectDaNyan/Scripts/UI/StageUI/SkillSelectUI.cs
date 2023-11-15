1e085351714b392a5e9a10a8e197cd1ff5600421
NewYorkKim <newyorkkim1030@gmail.com>
2023-11-15 20:13:14 +0900

    Improve: Skill Select UI 수정 (#125)
    * Fix: 프리펩 정리, 메뉴로 이동 버튼 안눌리는 문제 수정, unused override 삭제, BGM off(코드 수정할 때 신경쓰여서 임시로 꺼둠)
    * Fix: Skill Select Button 에러 띄우는 코드 삭제

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/SkillSelectUI.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/SkillSelectUI.cs
@@ -11,39 +11,16 @@ namespace ProjectDaNyan.Scripts.UI.StageUI
         private PlayerAttack _playerAttack;
         private PlayerBasicAttack _playerBasicAttack;
         private PlayerRandomFieldAttack _playerRandomFieldAttack;
-        [SerializeField]
-        private List<SkillElement> _skillElements;
-
         [SerializeField] private List<SkillSelectButton> _skillLevelUpButtons;
         
         private void Awake()
         {
             _playerAttack = FindObjectOfType<PlayerAttack>();
             _playerBasicAttack = FindObjectOfType<PlayerBasicAttack>();
-            foreach (SkillElement skillElement in _skillElements)
-            {
-                skillElement.gameObject.SetActive(false);
-            }
         }
 
         private void OnEnable()
         {
-            var count = 0;
-            //현재 가지고 있는 스킬 표시
-            //기본은 무조건 가지고 있음
-            _skillElements[count].SetImage("basicFire");
-            _skillElements[count].SetLevel(_playerAttack.basicFireLevel);
-            _skillElements[count].gameObject.SetActive(true);
-            count += 1;
-
-            if (_playerAttack.isField == true)
-            {
-                _skillElements[count].SetImage("field");
-                _skillElements[count].SetLevel(_playerRandomFieldAttack.randomFieldLevel);
-                _skillElements[count].gameObject.SetActive(true);
-                count += 1;
-            }
-            
             // 스킬 선택하기
             foreach (SkillSelectButton selectButton in _skillLevelUpButtons)
             {
