6e384cdf2635e01f0ff50215d1ae0b505fc38add
NewYorkKim <newyorkkim1030@gmail.com>
2023-11-20 12:16:32 +0900

    Add: PauseUI에서 Sound 조절 기능 추가
    Add: SkillInfoUI에서 PauseUI로 이동 추가

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/EffectSwitchHandler.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/UI/StageUI/EffectSwitchHandler.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SwitchHandler : MonoBehaviour
+public class EffectSwitchHandler : MonoBehaviour
 {
     [SerializeField] private Image on;
     [SerializeField] private Image off;
+    private SoundEffectController _soundEffectController;
     private int _switchIndex = 0;
+
+    private void Awake()
+    {
+        _soundEffectController = FindObjectOfType<SoundEffectController>();
+    }
 
     public void OnSwitchClicked()
     {
@@ -15,12 +21,14 @@ public class SwitchHandler : MonoBehaviour
         {
             on.gameObject.SetActive(false);
             off.gameObject.SetActive(true);
+            _soundEffectController.isSoundEffectPlayTemp = false;
             _switchIndex = 1;
         }
         else
         {
             on.gameObject.SetActive(true);
             off.gameObject.SetActive(false);
+            _soundEffectController.isSoundEffectPlayTemp = true;
             _switchIndex = 0;
         }
     }
