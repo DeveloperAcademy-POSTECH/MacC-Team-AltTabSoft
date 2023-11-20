67e016819045d8655b4dbe025f8804ad45d453ac
Kiyoung <rlaky45@naver.com>
2023-11-20 16:30:17 +0900

    Add: 플레이어에 대쉬 공격용 dashBullet 오브젝트 추가

--- ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
+++ ProjectDaNyan/Assets/ProjectDaNyan/Scripts/Space/Bullet.cs
@@ -8,7 +8,8 @@ public class Bullet : MonoBehaviour
     [SerializeField] private GameObject _bomb;
     [SerializeField] private GameObject _explosion;
 
-    public enum Type {
+    public enum Type
+    {
         //기본공격 
         Default,
         //초월공격 
@@ -24,7 +25,9 @@ public class Bullet : MonoBehaviour
         //드론공격 총알 
         Drone,
         //총알 피격 후 폭발 이펙트 
-        BulletExplosion
+        BulletExplosion,
+        //대쉬 공격 판정 총알
+        DashBullet
     }
 
 
@@ -34,7 +37,7 @@ public class Bullet : MonoBehaviour
     public int bombStack;
     //총알이 가진 트레일렌더러 
     TrailRenderer trail;
-    
+
     private void OnEnable()
     {
         switch (type)
@@ -54,14 +57,20 @@ public class Bullet : MonoBehaviour
             case Type.Drone:
                 damage = _attackStatus.droneDamage;
                 break;
+            case Type.DashBullet:
+                damage = _attackStatus.dashDamage;
+                break;
             default:
                 damage = 0f;
                 break;
         }
 
-        if(type != Type.BulletExplosion)
+        if (type != Type.BulletExplosion &&
+            type != Type.DashBullet)
             trail = GetComponent<TrailRenderer>();
-        if(type != Type.BombBlast)
+
+        if (type != Type.BombBlast &&
+            type != Type.DashBullet)
             StartCoroutine(Goodbye());
     }
 
@@ -69,7 +78,9 @@ public class Bullet : MonoBehaviour
     //총알이 어디에도 안맞았을 경우 일정시간 후 오브젝트풀로 회수 
     IEnumerator Goodbye()
     {
-        if (type != Type.BulletExplosion && type != Type.Laser && type != Type.BombBlast)
+        if (type != Type.BulletExplosion &&
+            type != Type.Laser &&
+            type != Type.BombBlast)
         {
             yield return new WaitForSeconds(1f);
             trail.Clear();
@@ -100,34 +111,46 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //총알이 몬스터에 닿을 경우의 로직 
-        if (other.gameObject.layer == LayerMask.NameToLayer("Monster") && type != Type.BombBlast)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Monster") &&
+            type != Type.BombBlast)
         {
             //오브젝트풀 회수하기 전에 트레일 미리 제거
-            if(type != Type.Laser && type != Type.BulletExplosion)
+            if (type != Type.Laser &&
+                type != Type.BulletExplosion &&
+                type != Type.DashBullet)
                 trail.Clear();
 
             //총알이 몬스터에 닿았을 때 터지는 이펙트
-            if(type == Type.Upgrade || type == Type.Default || type == Type.Drone) //몬스터 피격시 폭발 효과낼 총알 타입 조건 추가 필
+            if (type == Type.Upgrade ||
+               type == Type.Default ||
+               type == Type.Drone) //몬스터 피격시 폭발 효과낼 총알 타입 조건 추가 필
             {
                 StartCoroutine(BulletExplosion());
             }
 
             //총알 오브젝트풀로 회수 
-            if(type != Type.BulletExplosion)
+            if (type != Type.BulletExplosion &&
+                type != Type.DashBullet)
                 ObjectPoolManager.Inst.DestroyObject(this.gameObject);
-            
+
 
             //폭탄총알이 몹에게 닿으면 몹에게 폭탄이 부착됨
-            if(type == Type.Bomb && other != null && (other.transform.Find("BombOnMonster") == null || other.transform.Find("BombOnMonster").gameObject.activeSelf == false))
+            if (type == Type.Bomb &&
+                other != null &&
+                (other.transform.Find("BombOnMonster") == null || other.transform.Find("BombOnMonster").gameObject.activeSelf == false))
             {
-                GameObject InstantBomb = ObjectPoolManager.Inst.BringObject(_bomb);         
+                GameObject InstantBomb = ObjectPoolManager.Inst.BringObject(_bomb);
                 InstantBomb.transform.parent = other.transform;
                 InstantBomb.transform.position = other.transform.position + new Vector3(0, 2.5f, 0);
             }
         }
 
         //총알이 맵 바닥에 닿으면 사라지는 경우
-        if ((other.gameObject.layer == LayerMask.NameToLayer("Map_Object") || other.gameObject.layer == LayerMask.NameToLayer("Map_Floor")) && type != Type.Laser && type != Type.BombBlast && type != Type.BulletExplosion)
+        if ((other.gameObject.layer == LayerMask.NameToLayer("Map_Object") || other.gameObject.layer == LayerMask.NameToLayer("Map_Floor")) &&
+            type != Type.Laser &&
+            type != Type.BombBlast &&
+            type != Type.BulletExplosion &&
+            type != Type.DashBullet)
         {
             trail.Clear();
             ObjectPoolManager.Inst.DestroyObject(this.gameObject);
